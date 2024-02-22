ef46450d38b9f79412d10aac41686240bdb04709
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-02-22 21:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async () => {
+      const text = document.getElementById('text').value;
+      const delay = document.getElementById('delay').valueAsNumber;
+
+      await displayMessage(text, delay);
+    });
+
+    async function displayMessage(text, delay) {
+      await new Promise(resolve => setTimeout(resolve, delay));
+      document.getElementById('output').innerText = text;
+    }
